4534522ad2d7347eb130c7cc5e03252049f43cb5
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-15 14:11:40 +0200

    correct mistakes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,10 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
+  if (!sourceString) {
+    return {};
+  }
+
   return sourceString.split(';').reduce(addStyle, {});
 }
 
 function addStyle(styles, string) {
+  if (!string.includes(':')) {
+    return styles;
+  }
+
   const style = string.split(':').map((item) => item.trim());
 
   styles[style[0]] = style[1];
